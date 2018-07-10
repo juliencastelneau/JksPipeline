bfa9a91340e83073b930f94a2ef4a140737adb38
Julien Castelneau <julien.castelneau@ihu-liryc.fr>
2018-07-10 16:54:35 +0200

    Try to use checkout scm from pipeline steps plugin

--- Jenkinsfile
+++ Jenkinsfile
@@ -8,12 +8,13 @@ node('music-ubuntu-1204-64')
 	stage('parseJSON') {
 	    
 	    echo '### Lets go to parse'
-	    
-	    def loadFile = load("parseJSON.groovy")
+	    def scmVars = checkout scm
+
+	    //def loadFile = load("parseJSON.groovy")
 
     	// Call the method we defined in externalMethod.
-    	json_file = loadFile.parseJSON()
-	    echo "${json_file.release.medinriapublic.branch}"
+    	//json_file = loadFile.parseJSON()
+	    //echo "${json_file.release.medinriapublic.branch}"
 	} 
 }
 
--- parseJson.groovy
+++ parseJson.groovy
@@ -1,15 +1,9 @@
-if ("${params.configuration}" == "release")
-{
-    git branch: 'master', credentialsId: 'c88d1aef-98c3-4992-a205-31c13e56f012', url: 'https://github.com/FBU-IHU-LIRYC/music-utilities.git'
-}
-else if ("${params.configuration}" == "juliencastelneau")
-{
-    git branch: 'master', credentialsId: '89af51cf-1cc3-452b-8dd6-c057f600ac18', url: 'https://github.com/juliencastelneau/music-utilities.git'
-}
+git branch: 'master', credentialsId: '89af51cf-1cc3-452b-8dd6-c057f600ac18', url: 'https://github.com/juliencastelneau/JksPipeline.git'
+
 
 // Methods in this file will end up as object methods on the object that load returns.
 def parseJSON() {
-    dir ('jenkins-config-files')
+    dir ('.')
     {
         def json_file = readJSON file: 'config.json'
         return json_file
